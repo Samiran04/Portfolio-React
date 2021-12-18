b0853448f0acf71bae0237b1251bef04e9116e1e
Samiran Roy <samiranroy0407@gmail.com>
2021-12-19 02:23:35 +0530

    Home page in progress

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,9 +4,13 @@ class Home extends Component {
   render() {
     return (
       <div>
+        <div className="logo-div"></div>
         <div className="prof"></div>
+        <div className="prof-hide">Professionals</div>
         <div className="stud"></div>
+        <div className="stud-hide">Students</div>
         <div className="roles"></div>
+        <div className="roles-hide">Different Backgrounds</div>
       </div>
     );
   }

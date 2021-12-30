c9335a67d3a1581420da19b110473e9bb86b5a50
Samiran Roy <samiranroy0407@gmail.com>
2021-12-31 03:18:25 +0530

    Outlet

--- src/components/Outlet.js
+++ src/components/Outlet.js
@@ -2,7 +2,11 @@ import React, { Component } from "react";
 
 class Outlet extends Component {
   render() {
-    return <div className="outlet-cointainer"></div>;
+    return (
+      <div className="outlet-cointainer">
+        <div className="inner-box">Test</div>
+      </div>
+    );
   }
 }
 
